09949eb5d610d4d3f6f3a78e4b2550aadfa28e2f
Melissa O'Connor <mmoconnor23@gmail.com>
2016-12-20 22:44:17 -0500

    feat(solution): day 4, part 2

--- Day4.js
+++ Day4.js
@@ -3,9 +3,57 @@
 let fs = require('fs');
 let _ = require('lodash');
 
+let task = +process.argv[2];
+
 let input = fs.readFileSync('input/day4.txt', 'utf8');
 let rooms = input.split('\n');
+let shiftCypher = { 
+	a: 0,
+	b: 1, 
+	c: 2, 
+	d: 3, 
+	e: 4, 
+	f: 5,
+	g: 6,
+	h: 7,
+	i: 8,
+	j: 9,
+	k: 10,
+	l: 11,
+	m: 12, 
+	n: 13,
+	o: 14,
+	p: 15,
+	q: 16,
+	r: 17,
+	s: 18,
+	t: 19, 
+	u: 20, 
+	v: 21,
+	w: 22,
+	x: 23,
+	y: 24, 
+	z: 25
+};
+let invert = _.invert(shiftCypher);
+
 let password = 0;
+
+function shiftCode(room, sectorId) {
+	let newRoom = [];
+	
+	_.forEach(room, (letter, idx) => {
+		let letterCode = shiftCypher[letter];
+		let newLetterNum = (letterCode + sectorId) % 26;
+		newRoom.push(invert[newLetterNum]);
+	});
+
+	let codeWord = newRoom.join('');
+	if (_.includes(codeWord, 'north')) {
+		console.log('secret room, part 2: ', sectorId);
+	}
+	return newRoom.join('');
+}
 
 rooms.forEach((room) => {
 	//get checksum
@@ -14,11 +62,15 @@ rooms.forEach((room) => {
 
 	//get sector id
 	let idMatch = room.match(/\d+/);
-	let sectorId = idMatch[0];
+	let sectorId = +idMatch[0];
 	room = room.slice(0, idMatch.index);
 
 	//remove - from code
 	room = room.replace(/-/g, '');
+
+	if (task === 2) {
+		room = shiftCode(room, sectorId);
+	}
 
 	//create {letter: count, letter2: count}
 	let counts = _.countBy(room);
@@ -45,7 +97,7 @@ rooms.forEach((room) => {
 
 	//increase password by sectorId if valid
 	if (isValid) {
-		password += +sectorId;
+		password += sectorId;
 	}
 });
 
